844616d24a70be005dd2a2d04cc092377c3ef223
louisnovick <louisnovick@hotmail.com>
2015-02-19 17:06:06 -0500

    Tested angular routing and generated dynamic pages. Removed nav slider until we find a better solution

---
+++ source/javascripts/angapp.js
@@ -0,0 +1,93 @@
+	// create the module and name it scotchApp
+	var darwin = angular.module('darwin', ['ngRoute']);
+
+	// configure our routes
+	darwin.config(function($routeProvider) {
+		$routeProvider
+
+			.when('/', {
+				templateUrl : 'pages/home.html',
+				controller  : 'mainController'
+			})
+
+			.when('/peru', {
+				templateUrl : 'pages/peru.html',
+				controller  : 'peru'
+			})
+
+			.when('/riodejaneiro', {
+				templateUrl : 'pages/riodejaneiro.html',
+				controller  : 'riodejaneiro'
+			})
+
+			.when('/santacruz', {
+				templateUrl : 'pages/santacruz.html',
+				controller  : 'santacruz'
+			})
+
+			.when('/straitofmagellan', {
+				templateUrl : 'pages/straitofmagellan.html',
+				controller  : 'straitofmagellan'
+			})
+
+			.when('/tahitinewzealand', {
+				templateUrl : 'pages/tahitinewzealand.html',
+				controller  : 'tahitinewzealand'
+			})
+
+			.when('/tierradelfuego', {
+				templateUrl : 'pages/tierradelfuego.html',
+				controller  : 'tierradelfuego'
+			});
+
+	});
+
+	// create the controller and inject Angular's $scope
+	darwin.controller('mainController', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Home';
+	});
+
+	darwin.controller('peru', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Peru';
+	});
+
+	darwin.controller('riodejaneiro', function($scope) {
+		$scope.message = 'Rio de Janeiro';
+	});
+
+	darwin.controller('santacruz', function($scope) {
+		$scope.message = 'Santa Cruz';
+	});
+
+	darwin.controller('straitofmagellan', function($scope) {
+		// create a message to display in our view
+		$scope.message = 'Strait of Magellan';
+	});
+
+	darwin.controller('tahitinewzealand', function($scope) {
+		$scope.message = 'Tahiti and New Zealand';
+	});
+
+	darwin.controller('tierradelfuego', function($scope) {
+		$scope.message = 'Tierra Del Fuego';
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
