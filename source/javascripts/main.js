6bcb235b0ab43110e195431341157e5d31255271
mishingo <mr@mishingo.com>
2015-03-01 16:59:06 -0500

    add animation to slide nav

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -3,6 +3,22 @@ $().ready(function() {
 		videoId: 'XREElnb_wlQ',
 		start: 4
 	}); 
+
+	$('.slide-nav').mouseenter(function(){
+		$( this ).css( "margin", "0px 0px" );
+		$( this ).css("-webkit-transition","all 0.3s");
+		$( this ).css("-moz-transition","all 0.3s");
+		$( this ).css("-o-transition","all 0.3s");
+		$( this ).css("transition","all 0.3s");
+	});
+
+	$('.slide-nav').mouseleave(function(){
+		$( this ).css( "margin", "0px -110px" );
+		$( this ).css("-webkit-transition","all 0.3s");
+		$( this ).css("-moz-transition","all 0.3s");
+		$( this ).css("-o-transition","all 0.3s");
+		$( this ).css("transition","all 0.3s");
+	});
 });
 
 
